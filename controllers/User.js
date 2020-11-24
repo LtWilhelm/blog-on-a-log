a2e6e829d45b763448c05fd21402a176202f9f51
Mason Short <ironicallyshort737@hotmail.com>
2020-11-23 20:01:36 -0700

    User routes finished

--- controllers/User.js
+++ controllers/User.js
@@ -3,12 +3,28 @@ const { User } = require('../models/index');
 module.exports({
   Register: async ({ body }, res) => {
     try {
-      await User.create(body);
+      const user = new User(body);
+      user.password = user.generateHash(body.password);
+      user.save()
       res.sendStatus(200);
     }
     catch (err) {
       console.error(err);
       res.status(500).json(err);
     }
+  },
+  Login: async ({ body }, res) => {
+    try {
+      const user = await User.findOne({$or: {username: body.signInName, email: body.signInName}});
+      res.json(new VisibleUser(user.toObject()));
+    } catch (err) {
+      console.error(err);
+      res.status(500).json(err);
+    }
   }
 })
+
+function VisibleUser ({username, email}) {
+  this.username = username;
+  this.email = email;
+}
